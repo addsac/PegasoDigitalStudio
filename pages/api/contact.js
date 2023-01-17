7c27d17bf9c1d37730a71022b41486535a078ecf
Leonardo Citton <>
2023-01-17 16:01:28 +0100

    edited api

--- pages/api/contact.js
+++ pages/api/contact.js
@@ -4,9 +4,80 @@ mail.setApiKey(process.env.SENDGRID_API_KEY)
 
 export default async function(req, res) {
     const query = req.query;
+    const { source } = query
     const { interest, email, name, phone, budget, message, list, question1, question2, question3 } = query
 
     let fullText = ''
+
+    /* 
+     * Form from Pegaso Informatica
+    */
+
+    if(source == 'pegaso-informatica'){
+        if(interest == 1){
+            fullText = `
+                interesse: Hardware <br />
+                nome: ${name} <br />
+                email: ${email} <br />
+                messaggio: ${message} <br />
+            `
+
+            const data = {
+                to: 'paolo@pegasoinformatica.com',
+                from: 'pegasodigitalstudio@gmail.com',
+                subject: `Nuovo messaggio di ${name} - ${email}`,
+                text: fullText,
+                html: fullText.replace(/\r\n/g, '<br />'),
+            }
+
+            await mail.send(data)
+                .catch((err) => console.log(err.response.body))
+        }
+        if(interest == 2){
+            fullText = `
+                interesse: Reti e sicurezza <br />
+                nome: ${name} <br />
+                email: ${email} <br />
+                messaggio: ${message} <br />
+            `
+
+            const data = {
+                to: 'davide@pegasoinformatica.com',
+                from: 'pegasodigitalstudio@gmail.com',
+                subject: `Nuovo messaggio di ${name} - ${email}`,
+                text: fullText,
+                html: fullText.replace(/\r\n/g, '<br />'),
+            }
+
+            await mail.send(data)
+                .catch((err) => console.log(err.response.body))
+        }
+        if(interest == 3){
+            fullText = `
+                interesse: Software <br />
+                nome: ${name} <br />
+                email: ${email} <br />
+                messaggio: ${message} <br />
+            `
+
+            const data = {
+                to: 'info@pegasodigitalstudio.com',
+                from: 'pegasodigitalstudio@gmail.com',
+                subject: `Nuovo messaggio di ${name} - ${email}`,
+                text: fullText,
+                html: fullText.replace(/\r\n/g, '<br />'),
+            }
+
+            await mail.send(data)
+                .catch((err) => console.log(err.response.body))
+        }
+
+        return res.status(200).json({ status: 'OK' })
+    }
+
+    /* 
+     * Form from Pegaso Digital Studio
+    */
 
     if(interest == 0){
         fullText = `
