db67aab4b261565596baef0e0de66e272833a794
Leonardo Citton <>
2023-02-09 11:09:49 +0100

    edited pegaso-informatica work

--- components/BlogSwiper.jsx
+++ components/BlogSwiper.jsx
@@ -50,7 +50,7 @@ export default function BlogSwiper({ type, array }){
                             {/* Date */}
                             { type == 'articles' && (
                                 <p className="absolute top-6 left-6 px-3 py-2 rounded-full bg-white/60 backdrop-blur-md text-slate-900 text-xs"> 
-                                    { item.date != '' ? '16 Novembre 2022' : ''}
+                                    { item.date != '' ? item.date : '16 Novembre 2022'}
                                 </p>
                             )}
                             
---
+++ util/store/works.js
@@ -0,0 +1,24 @@
+const works = {
+    data: [
+        // Website
+        {
+            photo: '/img/lavori/marco-baldassa3.jpg',
+            title: 'Architettura – Marco Baldassa',
+            slug: '/lavori/marco-baldassa',
+        },
+        {
+            photo: '/img/lavori/pegaso-informatica/1.jpg',
+            title: 'Pegaso Informatica – Il Negozio',
+            slug: '/lavori/pegaso-informatica',
+        },
+
+        // Social
+        {
+            photo: '/img/lavori/sara-caponigro/2.jpg',
+            title: 'Sara Caponigro – Medico di Famiglia',
+            slug: '/lavori/sara-caponigro',
+        },
+    ],
+}
+
+export default works
