72dd75725da6037f34ac83cccac3806944a8138d
Leonardo Citton <>
2023-02-27 11:40:58 +0100

    edited hevoluta case study

--- app/lavori/hevoluta/page.jsx
+++ app/lavori/hevoluta/page.jsx
@@ -207,8 +207,12 @@ export default function Page({ params }) {
 
                     <div className="w-full h-auto bg-slate-900">
                         <div className="relative flex px-8 md:px-12 lg:px-16"> {/* gap-x-6 px-8 md:px-12 lg:px-16 */}
-                            <div className="w-full">
-                                <Image src="/img/lavori/hevoluta/1.jpg" width="800" height="800" alt="" className="h-auto w-full bg-slate-200" />
+                            <div className="grid grid-cols-12 gap-x-6">
+                                <div className="col-span-2"></div>
+                                <div className="col-span-8">
+                                    <Image src="/img/lavori/hevoluta/1.jpg" width="800" height="800" alt="" className="h-auto w-full bg-slate-200" />
+                                </div>
+                                <div className="col-span-2"></div>
                             </div>
                         </div>
                     </div>
