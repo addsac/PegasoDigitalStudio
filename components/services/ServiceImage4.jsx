d609a369c6b5ccfdfe07f27939cb105830350880
Leonardo Citton <>
2023-03-09 10:35:37 +0100

    servizi / branding page done.

--- components/services/ServiceImage4.jsx
+++ components/services/ServiceImage4.jsx
@@ -7,8 +7,8 @@ export default function ServiceImage4({ id, array = [] }) {
         className="mx-auto w-full max-w-[1440px] grid grid-cols-12 md:gap-x-16 px-8 md:px-12 lg:px-16 relative"
     >
 
-        { array.map((item) => (
-            <div className="col-span-12 md:col-span-6 lg:col-span-3 flex flex-col mb-12">
+        { array.map((item, index) => (
+            <div key={'service-image-4-'+index} className="col-span-12 md:col-span-6 lg:col-span-3 flex flex-col mb-12">
                 <div className="w-full h-[250px] overflow-hidden relative">
                     {/* <div className="absolute h-full w-full bg-[radial-gradient(ellipse_at_center,_var(--tw-gradient-stops))] from-transparent via-slate-900/80 to-slate-900"></div> */}
                     <Image 
