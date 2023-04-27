434e60b8e6f3d9d46a96207bf05d5f371198adc4
Leonardo Citton <>
2023-04-27 15:29:18 +0200

    debug

--- app/head.jsx
+++ app/head.jsx
@@ -10,7 +10,13 @@ export default function Head() {
       <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       <link rel="icon" href="/favicon.ico" />
 
-      <Script type="text/javascript" src="https://www.clarity.ms/tag/gugoq3zeit" strategy="afterInteractive" />
+      <Script id="clarity" strategy="lazyOnload" dangerouslySetInnerHTML={{ __html: `
+        (function(c,l,a,r,i,t,y){
+          c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+          t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+          y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+        })(window, document, "clarity", "script", "gugoq3zeit");
+      `}} />
     </>
   )
 }
