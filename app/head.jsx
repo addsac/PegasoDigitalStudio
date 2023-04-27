8924b084ee8c6e87b7e98242a733e568c80ae06b
Leonardo Citton <>
2023-04-27 15:35:26 +0200

    debug 2

--- app/head.jsx
+++ app/head.jsx
@@ -10,12 +10,26 @@ export default function Head() {
       <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       <link rel="icon" href="/favicon.ico" />
 
+      {/* Clarity */}
       <Script id="clarity" strategy="lazyOnload" dangerouslySetInnerHTML={{ __html: `
         (function(c,l,a,r,i,t,y){
           c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
           t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
           y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
         })(window, document, "clarity", "script", "gugoq3zeit");
+      `}} />
+
+      {/* Google analitycs */}
+      <Script id="analitycs" strategy="lazyOnload" dangerouslySetInnerHTML={{ __html: `
+        <!-- Google tag (gtag.js) -->
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-F0EF7ZBFTH"></script>
+        <script>
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+        
+          gtag('config', 'G-F0EF7ZBFTH');
+        </script>
       `}} />
     </>
   )
